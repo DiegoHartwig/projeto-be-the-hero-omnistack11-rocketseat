a49595124ea16c5ed50c2aa6f3b85213b3a15c8a
Diego Hartwig <hartwig.diego@gmail.com>
2020-03-28 09:56:41 -0300

    Implementando front end

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+
+import imgLogo from '../../assets/logo.svg';
+import imgHeroes from '../../assets/heroes.png';
+
+export default function Logon() {
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={imgLogo} alt="Be The Hero" />
+
+                <form>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="Informe sua ID" />
+
+                    <button className="button" type="submit">Entrar</button>
+
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+
+            <img src={imgHeroes} alt="Heroes" />
+        </div>
+    );
+}
